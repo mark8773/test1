64a667eb39b544480e1213e61de3b1f50ff9fc54
mark8773 <158565946+mark8773@users.noreply.github.com>
2024-09-15 00:27:01 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
+echo -e "\nTypes: deb\nURIs: http://ports.ubuntu.com/ubuntu-ports/\nSuites: noble noble-updates noble-backports\nComponents: main universe restricted multiverse\nSigned-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg" >> /etc/apt/sources.list.d/ubuntu.sources
 apt update
 apt upgrade -y
-echo -e "\nTypes: deb-src\nURIs: http://ports.ubuntu.com/ubuntu-ports/\nSuites: noble noble-updates noble-backports\nComponents: main universe restricted multiverse\nSigned-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg" >> /etc/apt/sources.list.d/ubuntu.sources
 apt-get build-dep mesa
 cat /etc/apt/sources.list.d/ubuntu.sources
