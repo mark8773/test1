abbeed1f55db12c0cf088142a6562c7799af4a16
mark8773 <158565946+mark8773@users.noreply.github.com>
2024-09-15 10:21:03 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
-ls -la
-
+echo -e "\nTypes: deb-src\nURIs: http://ports.ubuntu.com/ubuntu-ports/\nSuites: noble noble-updates noble-backports\nComponents: main universe restricted multiverse\nSigned-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg" >> /etc/apt/sources.list.d/ubuntu.sources
+apt update
+apt upgrade -y
+apt install libxcb* -y
+apt install xcb* -y
+apt install meson ninja-build libarchive-dev tar -y
+apt-get build-dep mesa -y
+cd mesa
+meson build -Dplatforms=x11 -Dgallium-drivers= -Dfreedreno-kmds=kgsl,msm -Dvulkan-drivers=freedreno -D dri3=enabled -D egl=disabled -D gles2=disabled -D glvnd=disabled -D glx=disabled -D libunwind=disabled -D shared-glapi=disabled -Dshared-llvm=disabled -D microsoft-clc=disabled -D valgrind=disabled -D gles1=disabled
+DESTDIR="$PWD/build64/release" ninja -C build install
+tar -cvf build64.tar build64
