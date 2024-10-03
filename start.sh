c6be5e1b058921d77971844a2f3f644ec9ce85aa
mark8773 <158565946+mark8773@users.noreply.github.com>
2024-10-03 21:34:47 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,10 @@
-sudo bash -c 'echo "
-deb-src mirror+file:/etc/apt/apt-mirrors.txt jammy main restricted
-deb-src mirror+file:/etc/apt/apt-mirrors.txt jammy-updates main restricted
-deb-src mirror+file:/etc/apt/apt-mirrors.txt jammy universe
-deb-src mirror+file:/etc/apt/apt-mirrors.txt jammy-updates universe
-deb-src mirror+file:/etc/apt/apt-mirrors.txt jammy multiverse
-deb-src mirror+file:/etc/apt/apt-mirrors.txt jammy-updates multiverse
-deb-src mirror+file:/etc/apt/apt-mirrors.txt jammy-backports main restricted universe multiverse
-deb-src mirror+file:/etc/apt/apt-mirrors.txt jammy-security main restricted
-deb-src mirror+file:/etc/apt/apt-mirrors.txt jammy-security universe
-deb-src mirror+file:/etc/apt/apt-mirrors.txt jammy-security multiverse
-" >> /etc/apt/sources.list'
-sudo apt update
-sudo apt upgrade -y
-sudo apt install libxcb* -y
-sudo apt install xcb* -y
-sudo apt install meson ninja-build libarchive-dev tar git -y
-sudo apt-get build-dep mesa -y
+echo -e "\nTypes: deb-src\nURIs: http://ports.ubuntu.com/ubuntu-ports/\nSuites: noble noble-updates noble-backports\nComponents: main universe restricted multiverse\nSigned-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg" >> /etc/apt/sources.list.d/ubuntu.sources
+apt update
+apt upgrade -y
+apt install libxcb* -y
+apt install xcb* -y
+apt install meson ninja-build libarchive-dev tar git -y
+apt-get build-dep mesa -y
 git clone -b winlator_wsi-termux-x11 https://gitlab.freedesktop.org/Pipetto-crypto/mesa
 cd mesa
 meson build -Dplatforms=x11 -Dgallium-drivers= -Dfreedreno-kmds=kgsl -Dvulkan-drivers=freedreno -D dri3=enabled -D egl=disabled -D gles2=disabled -D glvnd=disabled -D glx=disabled -D libunwind=disabled -D shared-glapi=disabled -Dshared-llvm=disabled -D microsoft-clc=disabled -D valgrind=disabled -D gles1=disabled
