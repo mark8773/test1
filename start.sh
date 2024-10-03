84a691b631fb3afddf7998cf8f252b88fd19c899
mark8773 <158565946+mark8773@users.noreply.github.com>
2024-10-03 21:19:43 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ sudo apt install libxcb* -y
 sudo apt install xcb* -y
 sudo apt install meson ninja-build libarchive-dev tar git -y
 sudo apt-get build-dep mesa -y
-wget -b winlator_wsi-termux-x11 https://gitlab.freedesktop.org/Pipetto-crypto/mesa &&
+git clone -b winlator_wsi-termux-x11 https://gitlab.freedesktop.org/Pipetto-crypto/mesa
 cd mesa
 meson build -Dplatforms=x11 -Dgallium-drivers= -Dfreedreno-kmds=kgsl -Dvulkan-drivers=freedreno -D dri3=enabled -D egl=disabled -D gles2=disabled -D glvnd=disabled -D glx=disabled -D libunwind=disabled -D shared-glapi=disabled -Dshared-llvm=disabled -D microsoft-clc=disabled -D valgrind=disabled -D gles1=disabled
 DESTDIR="$PWD/build64/release" ninja -C build install
