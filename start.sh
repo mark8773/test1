f11e7315b795effc4a19084bb960f467864bd554
mark8773 <158565946+mark8773@users.noreply.github.com>
2024-10-03 20:58:04 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-echo -e "\nTypes: deb-src\nURIs: http://ports.ubuntu.com/ubuntu-ports/\nSuites: noble noble-updates noble-backports\nComponents: main universe restricted multiverse\nSigned-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg" >> /etc/apt/sources.list.d/ubuntu.sources
-apt update
-apt upgrade -y
-apt install libxcb* -y
-apt install xcb* -y
-apt install meson ninja-build libarchive-dev tar git -y
-apt-get build-dep mesa -y
+sudo echo -e "\nTypes: deb-src\nURIs: http://ports.ubuntu.com/ubuntu-ports/\nSuites: noble noble-updates noble-backports\nComponents: main universe restricted multiverse\nSigned-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg" >> /etc/apt/sources.list.d/ubuntu.sources
+sudo apt update
+sudo apt upgrade -y
+sudo apt install libxcb* -y
+sudo apt install xcb* -y
+sudo apt install meson ninja-build libarchive-dev tar git -y
+sudo apt-get build-dep mesa -y
 wget -b winlator_wsi-termux-x11 https://gitlab.freedesktop.org/Pipetto-crypto/mesa
 cd mesa
 meson build -Dplatforms=x11 -Dgallium-drivers= -Dfreedreno-kmds=kgsl -Dvulkan-drivers=freedreno -D dri3=enabled -D egl=disabled -D gles2=disabled -D glvnd=disabled -D glx=disabled -D libunwind=disabled -D shared-glapi=disabled -Dshared-llvm=disabled -D microsoft-clc=disabled -D valgrind=disabled -D gles1=disabled
