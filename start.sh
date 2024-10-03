ca0a17aa437c1ba837be84c706cf713d2f916a58
mark8773 <158565946+mark8773@users.noreply.github.com>
2024-10-03 21:04:25 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+sudo cat /etc/apt/sources.list
+exit
 sudo echo -e "\nTypes: deb-src\nURIs: http://ports.ubuntu.com/ubuntu-ports/\nSuites: noble noble-updates noble-backports\nComponents: main universe restricted multiverse\nSigned-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg" >> /etc/apt/sources.list.d/ubuntu.sources
 sudo apt update
 sudo apt upgrade -y
