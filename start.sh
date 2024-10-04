92f585f085d88cb14bb585015cb177c83a9060a2
mark8773 <158565946+mark8773@users.noreply.github.com>
2024-10-04 08:03:54 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,11 +5,11 @@ apt install libxcb* -y
 apt install xcb* -y
 apt install meson ninja-build libarchive-dev tar wget git -y
 apt-get build-dep mesa -y
-git clone https://gitlab.freedesktop.org/mesa/mesa
+git clone -b winlator_wsi-termux-x11 https://gitlab.freedesktop.org/Pipetto-crypto/mesa
 cd mesa
-git checkout cb7f816fc4ba1f5958429a90e541870cb730a6fd
-wget -O 1.patch https://github.com/alexvorxx/Zink-Mesa-Xlib/releases/download/v0.0.7/04-wsi-termux-x11-only-kgsl_fix2.patch
-git apply -v 1.patch
+git checkout 70176295bf7a4c582a8fb263f9d58e5106851c8f
+#wget -O 1.patch https://github.com/alexvorxx/Zink-Mesa-Xlib/releases/download/v0.0.7/04-wsi-termux-x11-only-kgsl_fix2.patch
+#git apply -v 1.patch
 meson setup build -Dplatforms=x11 -Dgallium-drivers= -Dfreedreno-kmds=kgsl -Dvulkan-drivers=freedreno -Ddri3=enabled -Degl=disabled -Dgles2=disabled -Dglvnd=disabled -Dglx=disabled -Dlibunwind=disabled -Dshared-glapi=disabled -Dshared-llvm=disabled -Dmicrosoft-clc=disabled -Dvalgrind=disabled -Dgles1=disabled
 DESTDIR="$PWD/build64/release" ninja -C build install
 tar -cvf build64.tar build64
