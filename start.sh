84822f9c62863740e94f2d751c6d26a93b851c4f
mark8773 <158565946+mark8773@users.noreply.github.com>
2024-10-05 10:42:26 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ apt install meson ninja-build libarchive-dev tar wget git -y
 apt-get build-dep mesa -y
 git clone https://gitlab.freedesktop.org/mesa/mesa
 cd mesa
-git checkout 70176295bf7a4c582a8fb263f9d58e5106851c8f
+git checkout ed2ec808b1a581cbe875a18e4a3a44e0dcbd26c0
 wget -O 1.patch https://raw.githubusercontent.com/MastaG/mesa-turnip-ppa/e31c35e9c46e510c2445c884da194c51f4eb2abe/turnip-patches/dri3.patch
 git apply -v 1.patch
 meson setup build -Dplatforms=x11 -Dgallium-drivers= -Dfreedreno-kmds=kgsl -Dvulkan-drivers=freedreno -Ddri3=enabled -Degl=disabled -Dgles2=disabled -Dglvnd=disabled -Dglx=disabled -Dlibunwind=disabled -Dshared-glapi=disabled -Dshared-llvm=disabled -Dmicrosoft-clc=disabled -Dvalgrind=disabled -Dgles1=disabled
