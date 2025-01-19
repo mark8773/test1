113db1e07923480f3544249e5b39b859eefc65ef
mark8773 <158565946+mark8773@users.noreply.github.com>
2025-01-20 00:21:07 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ apt install xcb* -y
 apt install meson ninja-build libarchive-dev tar -y
 apt-get build-dep mesa -y
 cd mesa
-meson build -Dplatforms=x11 -Dgallium-drivers= -Dfreedreno-kmds=kgsl,msm -Dvulkan-drivers=freedreno -D egl=disabled -D gles2=disabled -D glvnd=disabled -D glx=disabled -D dri3=enabled -D libunwind=disabled -D shared-glapi=disabled -Dshared-llvm=disabled -D microsoft-clc=disabled -D valgrind=disabled -D gles1=disabled
+meson build -Dplatforms=x11 -Dgallium-drivers= -Dfreedreno-kmds=kgsl,msm -Dvulkan-drivers=freedreno -D egl=disabled -D gles2=disabled -D glvnd=disabled -D glx=disabled -D libunwind=disabled -D shared-glapi=disabled -Dshared-llvm=disabled -D microsoft-clc=disabled -D valgrind=disabled -D gles1=disabled
 DESTDIR="$PWD/build64/release" ninja -C build install
 tar -cvf build64.tar build64
