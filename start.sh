89f604b8f9f759fba90baa092f92accfc1037f37
mark8773 <158565946+mark8773@users.noreply.github.com>
2025-01-20 12:31:11 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-echo -e "\nTypes: deb-src\nURIs: http://ports.ubuntu.com/ubuntu-ports/\nSuites: noble noble-updates noble-backports\nComponents: main universe restricted multiverse\nSigned-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg" >> /etc/apt/sources.list.d/ubuntu.sources
+echo -e "\nTypes: deb-src\nURIs: http://ports.ubuntu.com/ubuntu-ports/\nSuites: jammy jammy-updates jammy-backports\nComponents: main universe restricted multiverse\nSigned-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg" >> /etc/apt/sources.list.d/ubuntu.sources
 apt update
 apt upgrade -y
 apt install libxcb* -y
